7dafeab5c0cd3b0651100f1b94fb88c71642ad19
Abed Murad <a.murad@theahoyapp.com>
2023-06-18 16:49:21 +0300

    Create Custom Lint Detector to detect the use of material components, rather than our custom app components.

--- core/designsystem/build.gradle.kts
+++ core/designsystem/build.gradle.kts
@@ -6,12 +6,28 @@ plugins {
 
 
 android {
-
     namespace = "com.am.now.core.designsystem"
+
+    lint {
+        checkDependencies = true
+    }
 }
 
 dependencies {
+    lintPublish(project(":lint"))
+
     api(libs.androidx.compose.foundation)
-    api(libs.androidx.compose.material3)
+    api(libs.androidx.compose.foundation.layout)
     api(libs.androidx.compose.material.iconsExtended)
+    api(libs.androidx.compose.material3)
+    api(libs.androidx.compose.runtime)
+    api(libs.androidx.compose.ui.tooling.preview)
+    api(libs.androidx.compose.ui.util)
+
+    debugApi(libs.androidx.compose.ui.tooling)
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.coil.kt.compose)
+
+
 }
