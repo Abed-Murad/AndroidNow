bac983f7fe93241ff49c3d77110463efd59a6263
Abed Murad <a.murad@theahoyapp.com>
2023-06-19 16:58:55 +0300

    Test the stash issue after editing the script.

--- core/ui/src/test/java/com/am/now/ui/ExampleUnitTest.kt
+++ core/ui/src/test/java/com/am/now/ui/ExampleUnitTest.kt
@@ -11,7 +11,9 @@ import kotlin.test.assertEquals
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
-        assertEquals(4,
-            2 + 2)
+        assertEquals(
+            4,
+            2 + 2,
+        )
     }
 }
